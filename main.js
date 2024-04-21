3b13b708addc3e6b4434263063a1256c72e002bc
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 02:24:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/*Store a person’s name, and include some whitespace characters at the beginning and end of
+the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name
+once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+*/
+//const myName: string = " shoaib ";
+// \t used for tab/space
+//
+//const myName: string = "\tshoaib"
+//\n used for mew line
+var myName = "\nshoaib";
+//console.log(myName);
+console.log(myName.trim());
